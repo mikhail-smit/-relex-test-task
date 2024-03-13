886f94ecd13511390180e766e0681bea8dd6f9a3
mikhail <mikhail.medvedew@mail.ru>
2024-03-13 20:41:43 +0300

    fix typo

--- src/main/java/mikhail/task/models/User.java
+++ src/main/java/mikhail/task/models/User.java
@@ -27,5 +27,5 @@ public class User {
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private List<Role> role;
+    private List<Role> roles;
 }
