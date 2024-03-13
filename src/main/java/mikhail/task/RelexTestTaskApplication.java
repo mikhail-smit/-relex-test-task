6758fd51ae9ac414855e0338566129969287c31f
mikhail <mikhail.medvedew@mail.ru>
2024-03-13 09:06:20 +0300

    init

---
+++ src/main/java/mikhail/task/RelexTestTaskApplication.java
@@ -0,0 +1,12 @@
+package mikhail.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelexTestTaskApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RelexTestTaskApplication.class, args);
+	}
+
+}
