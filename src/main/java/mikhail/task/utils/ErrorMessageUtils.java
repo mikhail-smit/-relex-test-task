40649fe2a50a2f8b2a03042c3d2f375b5166c4ca
mikhail <mikhail.medvedew@mail.ru>
2024-03-14 09:58:10 +0300

    add error message utils

---
+++ src/main/java/mikhail/task/utils/ErrorMessageUtils.java
@@ -0,0 +1,16 @@
+package mikhail.task.utils;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.util.stream.Collectors;
+
+@Component
+public class ErrorMessageUtils {
+    public String createMessage(Errors errors) {
+        return errors.getFieldErrors().stream()
+                .map(error -> String.format("Error at: %s, Message: %s; ",
+                        error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.joining());
+    }
+}
