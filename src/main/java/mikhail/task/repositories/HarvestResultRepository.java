3787542fe6df6fc388420405b8a97a833ad3ebaf
mikhail <mikhail.medvedew@mail.ru>
2024-03-14 03:21:23 +0300

    add methods to repositories

--- src/main/java/mikhail/task/repositories/HarvestResultRepository.java
+++ src/main/java/mikhail/task/repositories/HarvestResultRepository.java
@@ -2,11 +2,17 @@ package mikhail.task.repositories;
 
 import mikhail.task.models.HarvestResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface HarvestResultRepository extends JpaRepository<HarvestResult, Integer> {
     List<HarvestResult> findAllByUserId(int id);
+
+    List<HarvestResult> findAllByAtMomentBetween(Date from, Date to);
+    List<HarvestResult> findAllByAtMomentBetweenAndUserId(Date from, Date to, int id);
 }
